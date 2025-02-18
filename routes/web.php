c286e7acb68637d2a180caad231918603a43db40
RanggaCasper <hyfanutama@gmail.com>
2025-02-18 21:38:06 +0700

    add middleware checkRole

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ Route::middleware('guest')->group(function () {
 Route::post('auth/logout', [\App\Http\Controllers\Auth\LogoutController::class, 'logout'])->middleware('auth')->name('logout');
 
 // Admin
-Route::prefix('admin')->as('admin.')->middleware('auth')->group(function () {
+Route::prefix('admin')->as('admin.')->middleware('auth','checkRole:admin')->group(function () {
     Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
 });
 
 // User
-Route::prefix('user')->as('user.')->middleware('auth')->group(function () {
+Route::prefix('user')->as('user.')->middleware('auth','checkRole:user')->group(function () {
     Route::get('/', [\App\Http\Controllers\User\DashboardController::class, 'index'])->name('dashboard');
 });
